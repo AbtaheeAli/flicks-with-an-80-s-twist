96d417dd9abcd58cc326706dec1b4ba4bdddd5a8
Abtahee Ali <abtaheeali@gmail.com>
2020-06-24 15:03:45 -0400

    Images are displayed on website

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 export class Movie extends Component {
   render() {
     const { title, poster_path, overview } = this.props
+
     return (
       <li className="list-group-item m-2">
         {title}
-        <span className="movieItem m-2">{poster_path}</span>
+        <span className="movieItem m-2">
+          <img src={poster_path} height="100" width="100" />
+        </span>
         <span className="movieItem m-2">{overview}</span>
       </li>
     )
