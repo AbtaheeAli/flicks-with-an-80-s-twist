14e373da1a7a5e8fb5c76b408418a11477a3b759
Abtahee Ali <abtaheeali@gmail.com>
2020-06-24 14:23:16 -0400

    Added Movie component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+class Movie extends Component {
+  render() {
+    return (
+      <article>
+        <li className="list-group-item">{this.props.name}</li>
+      </article>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <main className="container p-5">
-        <div class="jumbotron bg-info text-white">
-          <h1 class="display-4">Flicks With An 80's Twist</h1>
-          <p class="lead">
+        <div className="jumbotron bg-info text-white">
+          <h1 className="display-4">Flicks With An 80's Twist</h1>
+          <p className="lead">
             A simple movies API that displays information about 80's movies.
           </p>
         </div>
         <ul className="list-group">
-          <li className="list-group-item">Movie 1</li>
-          <li className="list-group-item">Movie 2</li>
-          <li className="list-group-item">Movie 3</li>
-          <li className="list-group-item">Movie 4</li>
-          <li className="list-group-item">Movie 5</li>
+          <Movie name="Movie 1" />
+          <Movie name="Movie 2" />
+          <Movie name="Movie 3" />
+          <Movie name="Movie 4" />
+          <Movie name="Movie 5" />
         </ul>
       </main>
     )
